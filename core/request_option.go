5c199c2fc60d4379ddb57f29d40e29897bac856d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-01-06 21:13:37 +0000

    SDK regeneration

--- core/request_option.go
+++ core/request_option.go
@@ -64,8 +64,8 @@ func (r *RequestOptions) cloneHeader() http.Header {
 	headers := r.HTTPHeader.Clone()
 	headers.Set("X-Fern-Language", "Go")
 	headers.Set("X-Fern-SDK-Name", "github.com/square/square-go-sdk")
-	headers.Set("X-Fern-SDK-Version", "v1.0.0")
-	headers.Set("User-Agent", "github.com/square/square-go-sdk/1.0.0")
+	headers.Set("X-Fern-SDK-Version", "v1.0.1")
+	headers.Set("User-Agent", "github.com/square/square-go-sdk/1.0.1")
 	return headers
 }
 
