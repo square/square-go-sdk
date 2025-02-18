407251701f8bd634db7dcdee27d30891929b590b
fern-support <115122769+fern-api[bot]@users.noreply.github.com>
2025-02-18 18:20:15 -0500

    Add catalog/alias.go

---
+++ catalog/alias.go
@@ -0,0 +1,13 @@
+package catalog
+
+// ImagesCreateRequest is an alias for CreateImagesRequest.
+type ImagesCreateRequest = CreateImagesRequest
+
+// ImagesUpdateRequest is an alias for UpdateImagesRequest.
+type ImagesUpdateRequest = UpdateImagesRequest
+
+// ObjectDeleteRequest is an alias for DeleteObjectRequest.
+type ObjectDeleteRequest = DeleteObjectRequest
+
+// ObjectGetRequest is an alias for GetObjectRequest.
+type ObjectGetRequest = GetObjectRequest
