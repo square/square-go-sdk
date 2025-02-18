732bf98fb4fe7b078d6320b6a7ecaed7757e4707
fern-support <115122769+fern-api[bot]@users.noreply.github.com>
2025-02-18 18:37:24 -0500

    Add payment aliases

---
+++ alias.go
@@ -0,0 +1,10 @@
+package square
+
+// PaymentsListRequest is an alias for ListPaymentsRequest.
+type PaymentsListRequest = ListPaymentsRequest
+
+// PaymentsGetRequest is an alias for GetPaymentsRequest.
+type PaymentsGetRequest = GetPaymentsRequest
+
+// PaymentsCancelRequest is an alias for CancelPaymentsRequest.
+type PaymentsCancelRequest = CancelPaymentsRequest
